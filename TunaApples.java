db2e3ff2cfd07cd77c8b2f9383f9fe2742bbab5b
Morgan <mbayer.school@gmail.com>
2015-07-07 16:48:12 -0700

    Added Apples object to TunaApple

--- TunaApples.java
+++ TunaApples.java
@@ -2,5 +2,7 @@ class TunaApples{
 	public static void main(String[] args){
 		Tuna tunaObject = new Tuna();
 		tunaObject.simpleMessage();
+		Apples applesObject = new Apples();
+		appleObject,simpleMessage();
 	}
 }
